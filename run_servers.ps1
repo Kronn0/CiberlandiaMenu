70f194268e8563c7e8fa1deb25c8bb791576b7ff
Kronn0 <luissanchezgarcia2000@gmail.com>
2025-03-12 08:32:29 +0000

    añadido script automatizado

---
+++ run_servers.ps1
@@ -0,0 +1,8 @@
+#preconssola
+Start-Process powershell -ArgumentList "-NoExit", "-Command npm install; npm install -g serve"
+
+# Abre la primera consola y ejecuta node /backend/server.js
+Start-Process powershell -ArgumentList "-NoExit", "-Command node backend/server.js"
+
+# Abre la segunda consola y ejecuta los comandos de Angular
+Start-Process powershell -ArgumentList "-NoExit", "-Command ng build --configuration production; cd dist/ciberlandia; serve -s -l 8080"
